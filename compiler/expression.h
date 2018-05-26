dbf95828a9aa96a76acf7a117ac1ee201dadeb5b
DeanWinchesterFlying <136380243@qq.com>
2018-05-26 10:52:54 +0800

    update expression.h

--- compiler/expression.h
+++ compiler/expression.h
@@ -51,7 +51,7 @@ struct Expr_:public Node_
 struct ParenExpr_:public Expr_
 {
     Expr expr;
-    ParenExpr_(Expr expr):expr(expr){}
+    ParenExpr_(Expr expr):expr(expr){ this->id = NODE_EXP_PAREN; }
 };
 
 struct BinaryOpExpr_:public Expr_
@@ -60,7 +60,9 @@ struct BinaryOpExpr_:public Expr_
     Expr left, right;
 
     BinaryOpExpr_(Expr left, BinaryOperator operator_, Expr right):left(left), right(right),
-                                                                   operator_(operator_){}
+                                                                   operator_(operator_){
+        this->id = NODE_EXP_BINARY;
+    }
 };
 
 struct UnaryOpExpr_:public Expr_
@@ -70,7 +72,9 @@ struct UnaryOpExpr_:public Expr_
     Expr expr;
 
     UnaryOpExpr_(Expr expr, UnaryOperator operator_, bool postfix):expr(expr), operator_(operator_),
-                                                                  postfix(postfix){}
+                                                                  postfix(postfix){
+        this->id = NODE_EXP_UNARY;
+    }
 };
 
 struct ConditionalExpr_:public Expr_
@@ -78,7 +82,9 @@ struct ConditionalExpr_:public Expr_
     Expr condition, true_, false_;
 
     ConditionalExpr_(Expr condition, Expr true_, Expr false_):condition(condition), true_(true_),
-                                                               false_(false_){}
+                                                               false_(false_){
+        this->id = NODE_EXP_CONDITIONAL;
+    }
 };
 
 struct AssignExpr_:public Expr_
@@ -88,7 +94,9 @@ struct AssignExpr_:public Expr_
     Expr expr;
 
     AssignExpr_(DeclRefExpr var, AssignOperator operator_, Expr expr):var(var), expr(expr),
-                                                                      operator_(operator_){}
+                                                                      operator_(operator_){
+        this->id = NODE_EXP_ASSIGN;
+    }
 };
 
 struct CallExpr_:public Expr_
@@ -96,7 +104,9 @@ struct CallExpr_:public Expr_
     DeclRefExpr func;
     list<Expr> args;
 
-    CallExpr_(DeclRefExpr func, list<Expr> &args):func(func), args(args){}
+    CallExpr_(DeclRefExpr func, list<Expr> &args):func(func), args(args){
+        this->id = NODE_EXP_CALL;
+    }
 };
 
 struct MemberExpr_:public Expr_
@@ -106,7 +116,9 @@ struct MemberExpr_:public Expr_
     string member;
 
     MemberExpr_(DeclRefExpr parent, string member, bool isPointer):parent(parent), isPointer(isPointer),
-                                                                   member(member){}
+                                                                   member(member){
+        this->id = NODE_EXP_MEMBER;
+    }
 };
 
 struct ArraySubscriptExpr_:public Expr_
@@ -114,7 +126,9 @@ struct ArraySubscriptExpr_:public Expr_
     DeclRefExpr array;
     Expr offset;
 
-    ArraySubscriptExpr_(DeclRefExpr array, Expr offset):array(array), offset(offset){}
+    ArraySubscriptExpr_(DeclRefExpr array, Expr offset):array(array), offset(offset){
+        this->id = NODE_EXP_ARRAYSUBSCRIPT;
+    }
 };
 
 
@@ -122,7 +136,9 @@ struct DeclRefExpr_:public Expr_
 {
     string name;
 
-    DeclRefExpr_(string name):name(name){}
+    DeclRefExpr_(string name):name(name){
+        this->id = NODE_EXP_DECLREF;
+    }
 };
 
 struct ImplicitCastExpr_:public Expr_
@@ -130,7 +146,9 @@ struct ImplicitCastExpr_:public Expr_
     Type castType;
     Expr expr;
 
-    ImplicitCastExpr_(Type castType, Expr expr):castType(castType), expr(expr){}
+    ImplicitCastExpr_(Type castType, Expr expr):castType(castType), expr(expr){
+        this->id = NODE_EXP_IMPLICITCAST;
+    }
 };
 
 
@@ -139,33 +157,41 @@ struct CStyleCastExpr_:public Expr_
     Type castType;
     Expr expr;
 
-    CStyleCastExpr_(Type castType, Expr expr):castType(castType), expr(expr){}
+    CStyleCastExpr_(Type castType, Expr expr):castType(castType), expr(expr){
+        this->id = NODE_EXP_CSTYLECAST;
+    }
 };
 
 struct IntLiteral_:public Expr_
 {
     long value;
-    IntLiteral_(long value):value(value){}
+    IntLiteral_(long value):value(value){
+        this->id = NODE_EXP_INTLITERAL;
+    }
 };
 
 struct CharLiteral_:public Expr_
 {
     char value;
-    CharLiteral_(char value):value(value){}
+    CharLiteral_(char value):value(value){
+        this->id = NODE_EXP_CHARLITERAL;
+    }
 };
 
 
 struct FloatLiteral_:public Expr_
 {
     double value;
-    FloatLiteral_(double value):value(value){}
+    FloatLiteral_(double value):value(value){
+        this->id = NODE_EXP_FLOATLITERAL;
+    }
 };
 
 struct StrLiteral_:public Expr_
 {
     string value;
-    StrLiteral_(string &value):value(value){}
-    StrLiteral_(char *value){ this->value = string(value);}
+    StrLiteral_(string &value):value(value){this->id = NODE_EXP_STRLITERAL; }
+    StrLiteral_(char *value){ this->value = string(value);this->id = NODE_EXP_STRLITERAL;}
 };
 
 
